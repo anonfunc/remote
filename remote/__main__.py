213e4368cba5a8342dc3324d3094b35f6e6de2eb
Dwight Holman <dholman@linkedin.com>
2013-12-23 19:57:22 -0800

    Roku and sonos support.

--- remote/__main__.py
+++ remote/__main__.py
@@ -1,8 +1,13 @@
-from bottle import route, run
+import os
+
+from bottle import route, run, request, abort, static_file
 
 import harmony.util
 import pyroku
 import requests
+
+from soco import SoCo
+from soco import SonosDiscovery
 
 from remote import config
 
@@ -21,6 +26,10 @@ def harmony_client():
   return harmony_client_obj
 
 roku_client = pyroku.Roku(config.roku_ip)
+
+@route('/')
+def index():
+  return "harmony, sonos, roku"
 
 @route('/harmony')
 def harmony_index():
@@ -50,7 +59,7 @@ def harmony_activity_start(id):
 
 @route('/roku')
 def roku():
-  return 'channels'
+  return 'channels, press/button, type/string'
 
 @route('/roku/channel')
 @route('/roku/channels')
@@ -62,8 +71,83 @@ def roku():
 def roku_launch(id):
   return requests.post('http://' + roku_client.roku_address + "/launch/" + id)
 
-try:
-  run(host='0.0.0.0', port=9090, debug=True)
-finally:
-  harmony_client.disconnect()
+@route('/roku/press')
+def roku_press_index():
+  return """
+Home
+Rev
+Fwd
+Play
+Select
+Left
+Right
+Down
+Up
+Back
+InstantReplay
+Info
+Backspace
+Search
+Enter
+Lit_*"""
 
+@route('/roku/press/<button>')
+def roku_press(button):
+  roku_client.keypress(button)
+
+@route('/roku/type/<string>')
+def roku_press(string):
+  buttons = [ 'Ltr_' + ch for ch in string ]
+  for button in buttons:
+    roku_client.keypress(button)
+
+@route('/sonos')
+def sonos_view():
+  speakers = {}
+  sonos_devices = SonosDiscovery()
+  for ip in sonos_devices.get_speaker_ips():
+    device = SoCo(ip)
+    info = device.get_speaker_info()
+    info['ip'] = ip
+    zone_name = info.get('zone_name', None)
+    if zone_name is not None:
+      speakers[zone_name] = info
+  return speakers
+
+@route('/sonos/<ip>')
+def sonos_view(ip):
+  device = SoCo(ip)
+  info = device.get_speaker_info()
+  info['ip'] = ip
+  return info
+
+@route('/sonos/<ip>/volume')
+def sonos_volume(ip):
+  device = SoCo(ip)
+  val = request.query.val
+  if val:
+    try:
+      val = int(val)
+    except:
+      abort(500, "Bad value")
+    return device.volume(val)
+  else:
+    return str(device.volume())
+
+#@route('/sonos/<ip>/<method_name>')
+#def sonos_method(ip, method_name):
+  #device = SoCo(ip)
+  #method = getattr(device, method_name)
+  #result = method()
+  #return result
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+  module_dir = os.path.dirname(__file__)
+  return static_file(filepath, root=os.path.join(module_dir, 'static'))
+
+try:
+  run(host='0.0.0.0', port=9090, debug=True, reloader=True)
+finally:
+  if harmony_client_obj is not None:
+    harmony_client().disconnect()
