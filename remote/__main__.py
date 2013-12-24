ed70ff66842f90c0b1c157174406527b2504466a
Dwight Holman <dholman@linkedin.com>
2013-12-23 19:55:46 -0800

    Harmony client lazy init, so we don't wait for it on startup.

--- remote/__main__.py
+++ remote/__main__.py
@@ -10,35 +10,41 @@ from remote import config
 def hello():
   return "Hello World!"
 
-harmony_client = harmony.util.get_client(config.harmony_ip,
-                                         config.harmony_port,
-                                         config.harmony_email,
-                                         config.harmony_password)
+harmony_client_obj = None
+def harmony_client():
+  global harmony_client_obj
+  if harmony_client_obj is None:
+    harmony_client_obj = harmony.util.get_client(config.harmony_ip,
+                                             config.harmony_port,
+                                             config.harmony_email,
+                                             config.harmony_password)
+  return harmony_client_obj
+
 roku_client = pyroku.Roku(config.roku_ip)
 
 @route('/harmony')
-def harmony():
+def harmony_index():
   return "config, activity"
 
 @route('/harmony/config')
 def harmony_config():
   import pprint
-  return pprint.pformat(harmony_client.get_config(), width=200)
+  return pprint.pformat(harmony_client().get_config(), width=200)
 
 @route('/harmony/activity')
 @route('/harmony/activities')
 def harmony_activity():
-  config = harmony_client.get_config()
+  config = harmony_client().get_config()
   activities = config['activity']
   return dict((a['label'], a['id']) for a in activities)
 
 @route('/harmony/activity/<id>')
 @route('/harmony/activities/<id>')
 def harmony_activity_start(id):
-  current = harmony_client.get_current_activity()
+  current = harmony_client().get_current_activity()
   if str(current) != str(id):
     print "Current activity is %s, want %s"
-    return harmony_client.start_activity(id)
+    return harmony_client().start_activity(id)
   else:
     return "Current activity is already %s" % id
 
